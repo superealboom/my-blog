0f6c8e6b8da0fb7f35f8bfd1a76399799c6f7974
tianci <superealboom@163.com>
2025-02-14 17:51:18 +0800

    修改显示联系方式

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -15,10 +15,8 @@ export default defineThemeConfig({
     { icon: {
         svg: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M17.25 2.75H6.75A4.75 4.75 0 0 0 2 7.5v9a4.75 4.75 0 0 0 4.75 4.75h10.5A4.76 4.76 0 0 0 22 16.5v-9a4.76 4.76 0 0 0-4.75-4.75m-3.65 8.32a3.26 3.26 0 0 1-3.23 0L3.52 7.14a3.25 3.25 0 0 1 3.23-2.89h10.5a3.26 3.26 0 0 1 3.23 2.89z"/></svg>'
       }, link: 'mailto:superealboom@163.com' },
-    { icon: 'github', link: 'https://github.com/superealboom' },
-    { icon: 'juejin', link: 'https://juejin.cn/user/2278612334544056' },
   ],
-  // navbarSocialInclude: ['github'], // 允许显示在导航栏的 social 社交链接
+  navbarSocialInclude: ['qq'], // 允许显示在导航栏的 social 社交链接
   // aside: true, // 页内侧边栏， 默认显示在右侧
   // outline: [2, 3], // 页内大纲， 默认显示 h2, h3
 
