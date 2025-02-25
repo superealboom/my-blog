a51b82f84f7006ac1765d3ec7d09e10f2baf1577
tianci <superealboom@163.com>
2025-02-25 09:34:39 +0800

    显示 4 级标题

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -18,7 +18,7 @@ export default defineThemeConfig({
   ],
   navbarSocialInclude: ['qq'], // 允许显示在导航栏的 social 社交链接
   // aside: true, // 页内侧边栏， 默认显示在右侧
-  // outline: [2, 3], // 页内大纲， 默认显示 h2, h3
+  outline: [2, 4], // 页内大纲， 默认显示 h2, h3
 
   /**
    * 文章版权信息
